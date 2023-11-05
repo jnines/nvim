6ab3e4d8a8bedc0615713dab5b093a71cbb286a6
jnines <jason.mullenberg@gmail.com>
2023-11-05 11:38:41 -0600

    Tweak contrast on colorscheme.

--- lua/o/plugins/colorscheme.lua
+++ lua/o/plugins/colorscheme.lua
@@ -9,6 +9,8 @@ return {
 		vim.g.gruvbox_material_enable_italic = 1
 		vim.g.gruvbox_material_dim_inactive_windows = 1
 		vim.g.gruvbox_material_show_eob = 0
+		vim.g.gruvbox_material_ui_contrast = "high"
+		vim.g.gruvbox_material_cursor_line_contrast = "higher"
 		vim.cmd("colorscheme gruvbox-material")
 		vim.api.nvim_set_hl(0, "Comment", { fg = "#8c8c8c" })
 	end,
