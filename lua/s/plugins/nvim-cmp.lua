c709651a906d0a0cf70aea056dd8cfda7eb5893d
jnines <jason.mullenberg@gmail.com>
2023-12-29 08:33:51 -0600

    Tweak luasnip setup/mappings

--- lua/s/plugins/nvim-cmp.lua
+++ lua/s/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@ return {
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-emoji",
 		"hrsh7th/cmp-nvim-lua",
-		"L3MON4D3/LuaSnip",
+		{ "L3MON4D3/LuaSnip", tag = "v2.*", run = "make install_jsregexp" },
 		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
 		"onsails/lspkind.nvim", -- vs-code pictograms
@@ -23,6 +23,12 @@ return {
 
 		require("luasnip.loaders.from_vscode").lazy_load()
 
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,noinsert",
@@ -33,8 +39,27 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
