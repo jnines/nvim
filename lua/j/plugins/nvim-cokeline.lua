81517077b6c53da02dc73135f57319e2c9b3a309
jnines <jason.mullenberg@gmail.com>
2023-12-31 19:34:13 -0600

    Tweak cokeline

--- lua/j/plugins/nvim-cokeline.lua
+++ lua/j/plugins/nvim-cokeline.lua
@@ -11,18 +11,21 @@ return {
 
 		local red = vim.g.terminal_color_1
 		local yellow = vim.g.terminal_color_3
+		local teal = vim.g.terminal_color_4
 
 		local components = {
 			space = {
 				text = " ",
 				truncation = { priority = 1 },
 			},
+
 			separator = {
 				text = function(buffer)
 					return buffer.index ~= 1 and "󱋱" or ""
 				end,
 				truncation = { priority = 1 },
 			},
+
 			devicon = {
 				text = function(buffer)
 					return (mappings.is_picking_focus() or mappings.is_picking_close()) and buffer.pick_letter .. " "
@@ -33,8 +36,11 @@ return {
 						or (mappings.is_picking_close() and red)
 						or buffer.devicon.color
 				end,
-				style = function(_)
-					return (mappings.is_picking_focus() or mappings.is_picking_close()) and "italic,bold" or nil
+				bold = function(_)
+					return mappings.is_picking_focus()
+				end,
+				italic = function(_)
+					return mappings.is_picking_focus()
 				end,
 				truncation = { priority = 1 },
 			},
@@ -44,7 +50,7 @@ return {
 					return buffer.unique_prefix
 				end,
 				fg = comments_fg,
-				style = "italic",
+				italic = true,
 				truncation = {
 					priority = 3,
 					direction = "left",
@@ -54,20 +60,18 @@ return {
 				text = function(buffer)
 					return buffer.filename
 				end,
-				fg = function(buffer)
-					return buffer.is_modified and red or ""
+				bold = function(buffer)
+					return buffer.is_focused
 				end,
-				style = function(buffer)
-					return ((buffer.is_focused and buffer.diagnostics.errors ~= 0) and "bold,underline")
-						or (buffer.is_focused and "bold")
-						or (buffer.diagnostics.errors ~= 0 and "underline")
-						or nil
+				underline = function(buffer)
+					return buffer.is_focused and buffer.diagnostics.errors ~= 0
 				end,
 				truncation = {
 					priority = 2,
 					direction = "left",
 				},
 			},
+
 			diagnostics = {
 				text = function(buffer)
 					return (buffer.diagnostics.errors ~= 0 and "  " .. buffer.diagnostics.errors)
@@ -81,14 +85,14 @@ return {
 				end,
 				truncation = { priority = 1 },
 			},
+
 			close_or_unsaved = {
 				text = function(buffer)
 					return buffer.is_modified and "●" or "󰅙"
 				end,
-				fg = function(_)
-					return red
+				fg = function(buffer)
+					return buffer.is_modified and red or teal
 				end,
-
 				delete_buffer_on_left_click = true,
 				truncation = { priority = 1 },
 			},
@@ -107,8 +111,8 @@ return {
 				filetype = { "NvimTree" },
 				components = {
 					{
-						text = function(buf)
-							return buf.filetype
+						text = function(_)
+							return "   File Exporer"
 						end,
 						fg = yellow,
 						bg = function()
@@ -122,9 +126,9 @@ return {
 				components.space,
 				components.separator,
 				components.space,
-				components.unique_prefix,
 				components.devicon,
 				components.space,
+				components.unique_prefix,
 				components.filename,
 				components.diagnostics,
 				components.space,
@@ -132,7 +136,7 @@ return {
 				components.close_or_unsaved,
 			},
 		})
-		vim.keymap.set("n", "<leader>bx", function()
+		vim.keymap.set("n", "<leader>bp", function()
 			mappings.pick("focus")
 		end, { desc = "Pick buffer to focus" })
 	end,
