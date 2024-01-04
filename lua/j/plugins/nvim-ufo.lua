139bb9b7593d6cbbd58e70acf13bd3c840768737
jnines <jason.mullenberg@gmail.com>
2024-01-04 16:26:50 -0600

    Tweak folds in ufo

--- lua/j/plugins/nvim-ufo.lua
+++ lua/j/plugins/nvim-ufo.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = "kevinhwang91/promise-async",
 	config = function()
 		local ufo = require("ufo")
-		vim.o.foldcolumn = "1"
+		vim.o.foldcolumn = "0"
 		vim.o.foldlevel = 99
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
@@ -18,7 +18,37 @@ return {
 				vim.lsp.buf.hover()
 			end
 		end, { desc = "Fold peek" })
+
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = ("   󰘕 %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
+		---@diagnostic disable-next-line: missing-fields
 		ufo.setup({
+			fold_virt_text_handler = handler,
 			preview = {
 				win_config = {
 					winblend = 20,
