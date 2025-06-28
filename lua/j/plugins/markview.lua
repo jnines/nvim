f955f923e8aad026ea7c656b323e2dd98804d039
jnines <jason.mullenberg@gmail.com>
2025-06-28 17:05:05 -0500

    Load treesitter before markview

--- lua/j/plugins/markview.lua
+++ lua/j/plugins/markview.lua
@@ -1,6 +1,9 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
 
 	vim.keymap.set("n", "<leader>mp", ":Markview toggle<CR>", { desc = "Markdown Preview" }),
 }
