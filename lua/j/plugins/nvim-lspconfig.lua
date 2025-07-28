38f618f768b15005ac2fcc5edef0c145b0c65a84
jnines <jason.mullenberg@gmail.com>
2025-07-28 08:42:50 -0500

    Clean up / Update lsp for 0.11

--- lua/j/plugins/nvim-lspconfig.lua
+++ lua/j/plugins/nvim-lspconfig.lua
@@ -2,173 +2,121 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		-- "hrsh7th/cmp-nvim-lsp",
 		"saghen/blink.cmp",
 		"b0o/schemastore.nvim",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "antosha417/nvim-lsp-file-operations" },
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-
 		local blink = require("blink.cmp")
+		require("lsp-file-operations").setup()
 
-		local basic_on_attach = function(_, bufnr)
-			-- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-			vim.diagnostic.config({ virtual_lines = { current_line = true } })
-			local km = vim.keymap
+		vim.diagnostic.config({
+			virtual_lines = {
+				current_line = true,
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = "󰋼 ",
+					[vim.diagnostic.severity.HINT] = "󰌵 ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
+		local km = vim.keymap
 
-			km.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", { desc = "Goto references" })
-			km.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
-			km.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Goto definitions" })
-			km.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Goto implementations" })
-			km.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Goto type definitions" })
-			km.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-			km.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-			km.set("v", "<leader>rs", 'y:%s/<C-r>"//<left>', { desc = "Rename section" })
-			km.set("n", "<leader>db", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Buffer diagnostics" })
-			km.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Line diagnostics" })
-			-- km.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic" })
-			-- km.set("n", "[d", "<cmd>vim.diagnostic.jump({count=-1})<CR>", { desc = "Goto previous diagnostic" })
-			-- km.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
-			km.set("n", "K", vim.lsp.buf.hover, { desc = "Selection documentation" })
-			km.set("n", "<leader>xx", ":LspRestart<CR>", { desc = "Restart LSP" })
-		end
+		km.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", { desc = "Goto references" })
+		km.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+		km.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Goto definitions" })
+		km.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Goto implementations" })
+		km.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Goto type definitions" })
+		km.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+		km.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+		km.set("v", "<leader>rs", 'y:%s/<C-r>"//<left>', { desc = "Rename section" })
+		km.set("n", "<leader>db", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Buffer diagnostics" })
+		km.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+		km.set("n", "K", vim.lsp.buf.hover, { desc = "Selection documentation" })
+		km.set("n", "<leader>xx", ":LspRestart<CR>", { desc = "Restart LSP" })
+		km.set("n", "<leader>L", "<ESC>wbywofmt.Println()<ESC>hp", { desc = "Log under cursor" })
+		km.set("n", "<leader>ge", "oif err != nil {<CR>}<ESC>Oreturn ", { desc = "Go Err" })
+		km.set("n", "<leader>L", "<ESC>wbywoconsole.log()<ESC>hp", { desc = "Log under cursor" })
 
-		local ts_on_attach = function(_, bufnr)
-			basic_on_attach(_, bufnr)
-			vim.keymap.set("n", "<leader>L", "<ESC>wbywoconsole.log()<ESC>hp", { desc = "Log under cursor" })
-		end
-
-		local go_on_attach = function(_, bufnr)
-			basic_on_attach(_, bufnr)
-			vim.keymap.set("n", "<leader>L", "<ESC>wbywofmt.Println()<ESC>hp", { desc = "Log under cursor" })
-			vim.keymap.set("n", "<leader>ge", "oif err != nil {<CR>}<ESC>Oreturn ", { desc = "Go Err" })
-		end
-
-		local capabilities = blink.get_lsp_capabilities()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities())
 		capabilities.textDocument.foldingRange = {
 			dynamicRegistration = false,
 			lineFoldingOnly = true,
 		}
+
 		local schemas = require("schemastore")
+		local servers = {
+			bashls = {},
+			html = {},
+			ts_ls = {},
+			cssls = {},
+			tailwindcss = {},
+			graphql = {},
+			emmet_ls = {},
+			pyright = {},
+			jsonls = {
+				settings = {
+					json = {
+						schemas = schemas.json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			},
+			yamlls = {
+				settings = {
+					yaml = {
+						schemaStore = { enable = false, url = "" },
+						schemas = schemas.yaml.schemas(),
+					},
+				},
+			},
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = { vim.env.VIMRUNTIME },
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+						completion = {
+							callSnipper = "Replace",
+						},
+					},
+				},
+			},
+			marksman = {},
+			gopls = {
+				settings = {
+					gopls = {
+						usePlaceholders = false,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			},
+			qmlls = { cmd = { "qmlls6" } },
+		}
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		for serv, cfg in pairs(servers) do
+			cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, cfg.capabilities or {})
+			vim.lsp.config(serv, cfg)
+			vim.lsp.enable(serv)
 		end
-
-		lspconfig.bashls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.html.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-			on_attach = ts_on_attach,
-		})
-
-		lspconfig.cssls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.tailwindcss.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.graphql.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.emmet_ls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.jsonls.setup({
-			settings = {
-				json = {
-					schemas = schemas.json.schemas(),
-					validate = { enable = true },
-				},
-			},
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.yamlls.setup({
-			settings = {
-				yaml = {
-					schemaStore = { enable = false, url = "" },
-					schemas = schemas.yaml.schemas(),
-				},
-			},
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = { vim.env.VIMRUNTIME },
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
-					completion = {
-						callSnipper = "Replace",
-					},
-				},
-			},
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.marksman.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.gopls.setup({
-			capabilities = capabilities,
-			on_attach = go_on_attach,
-			settings = {
-				gopls = {
-					usePlaceholders = false,
-					analyses = {
-						unusedparams = true,
-					},
-				},
-			},
-		})
-
-		lspconfig.qmlls.setup({
-			cmd = { "qmlls6" },
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.marksman.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
 	end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,84 +2,71 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		-- "hrsh7th/cmp-nvim-lsp",
 		"saghen/blink.cmp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "antosha417/nvim-lsp-file-operations" },
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-
 		local blink = require("blink.cmp")
+		require("lsp-file-operations").setup()
 
-		local basic_on_attach = function(_, bufnr)
-			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-			vim.diagnostic.config({ virtual_lines = { current_line = true } })
-			local km = vim.keymap
+		vim.diagnostic.config({
+			virtual_lines = {
+				current_line = true,
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = "󰋼 ",
+					[vim.diagnostic.severity.HINT] = "󰌵 ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
 
-			km.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", { desc = "Goto references" })
-			km.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
-			km.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Goto definitions" })
-			km.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Goto implementations" })
-			km.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Goto type definitions" })
-			km.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-			km.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-			km.set("v", "<leader>rs", 'y:%s/<C-r>"//<left>', { desc = "Rename section" })
-			km.set("n", "<leader>db", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Buffer diagnostics" })
-			km.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Line diagnostics" })
-			km.set("n", "K", vim.lsp.buf.hover, { desc = "Selection documentation" })
-			km.set("n", "<leader>xx", ":LspRestart<CR>", { desc = "Restart LSP" })
+		local km = vim.keymap
+
+		km.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", { desc = "Goto references" })
+		km.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+		km.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Goto definitions" })
+		km.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Goto implementations" })
+		km.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Goto type definitions" })
+		km.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+		km.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+		km.set("v", "<leader>rs", 'y:%s/<C-r>"//<left>', { desc = "Rename section" })
+		km.set("n", "<leader>db", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Buffer diagnostics" })
+		km.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+		km.set("n", "K", vim.lsp.buf.hover, { desc = "Selection documentation" })
+		km.set("n", "<leader>xx", ":LspRestart<CR>", { desc = "Restart LSP" })
+		km.set("n", "<leader>L", "<ESC>wbywoconsole.log()<ESC>hp", { desc = "Log under cursor" })
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities())
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+
+		local servers = {
+			bashls = {},
+			html = {},
+			ts_ls = {},
+			emmet_ls = {},
+			pyright = {},
+			jsonls = {},
+			yamlls = {},
+			marksman = {},
+		}
+
+		for serv, cfg in pairs(servers) do
+			cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, cfg.capabilities or {})
+			vim.lsp.config(serv, cfg)
+			vim.lsp.enable(serv)
 		end
-
-		local ts_on_attach = function(_, bufnr)
-			basic_on_attach(_, bufnr)
-			vim.keymap.set("n", "<leader>L", "<ESC>wbywoconsole.log()<ESC>hp", { desc = "Log under cursor" })
-		end
-
-		local capabilities = blink.get_lsp_capabilities()
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
-		lspconfig.bashls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.html.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-			on_attach = ts_on_attach,
-		})
-
-		lspconfig.emmet_ls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.jsonls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.yamlls.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
-
-		lspconfig.marksman.setup({
-			capabilities = capabilities,
-			on_attach = basic_on_attach,
-		})
 	end,
 }
