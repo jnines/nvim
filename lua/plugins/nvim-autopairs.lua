20db948fe4c31e1df1a6fa63ff98c9917b7be250
jnines <jason.mullenberg@gmail.com>
2025-08-23 20:37:50 -0500

    Switch to mini.pairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,9 +1,6 @@
 return {
   'windwp/nvim-autopairs',
   event = { 'InsertEnter' },
-  dependencies = {
-    'hrsh7th/nvim-cmp',
-  },
   config = function()
     local autopairs = require('nvim-autopairs')
 
