bd512eeedd2aedcf8ea1b134d25f1b6ea572b6b8
jnines <jason.mullenberg@gmail.com>
2026-02-01 18:25:16 -0600

    Set workspace file path/create a better notification then default

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -1,3 +1,24 @@
 return {
   'natecraddock/workspaces.nvim',
+  config = function()
+    --** Workspace file location **--
+    local ws_file = '~/.local/workspaces'
+
+    local workspaces = require('workspaces')
+    workspaces.setup({
+      path = vim.fs.normalize(ws_file),
+      hooks = {
+        open = function()
+          local ws_name = workspaces.name()
+          local ws_path = workspaces.path()
+
+          vim.notify(
+            (' Entered Project: %s\n %s'):format(ws_name, ws_path),
+            vim.log.levels.INFO,
+            { title = 'Workspace' }
+          )
+        end,
+      },
+    })
+  end,
 }
