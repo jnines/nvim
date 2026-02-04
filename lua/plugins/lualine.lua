00022d7bbba903220939afc226553dcbf4c554dc
jnines <jason.mullenberg@gmail.com>
2026-02-03 20:06:56 -0600

    Convert lualine to opts

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,46 +1,35 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local lualine = require('lualine')
-    local lazy_status = require('lazy.status')
-    lualine.setup({
-      options = {
-        theme = 'auto',
-        disabled_filetypes = {
-          statusline = { 'alpha', 'NvimTree' },
-          winbar = {
-            'help',
-            'alpha',
-            'dashboard',
-            'neogitstatus',
-            'NvimTree',
-            'Trouble',
-            'lir',
-            'Outline',
-            'spectre_panel',
-            'toggleterm',
-          },
-          NvimTree = {},
+  opts = {
+    options = {
+      theme = 'auto',
+      disabled_filetypes = {
+        statusline = { 'alpha', 'NvimTree' },
+        winbar = {
+          'help',
+          'lir',
+          'Outline',
+          'snacks_layout_box',
         },
       },
-      sections = {
-        lualine_c = {
-          { 'filesize' },
-          { 'filename', path = 3 },
-        },
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = '#9c814f' },
-          },
-          { 'encoding' },
-          { 'fileformat' },
-          { 'filetype' },
-        },
+    },
+    sections = {
+      lualine_c = {
+        { 'filesize' },
+        { 'filename', path = 3 },
       },
-      extensions = { 'lazy' },
-    })
-  end,
+      lualine_x = {
+        {
+          require('lazy.status').updates,
+          cond = require('lazy.status').has_updates,
+          color = { fg = '#9c814f' },
+        },
+        { 'encoding' },
+        { 'fileformat' },
+        { 'filetype' },
+      },
+    },
+    extensions = { 'lazy' },
+  },
 }
