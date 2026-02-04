6f4a6bff5688571030600cf9be7005ca44ad4c9b
jnines <jason.mullenberg@gmail.com>
2026-02-04 09:26:03 -0600

    Markdown preview to opt

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -3,15 +3,12 @@ return {
   'OXY2DEV/markview.nvim',
   lazy = false,
 
-  vim.keymap.set('n', '<leader>mp', ':Markview toggle<CR>', { desc = 'Markdown Preview' }),
-
-  config = function()
-    local mkv = require('markview')
-
-    mkv.setup({
-      experimental = {
-        check_rtp_message = false,
-      },
-    })
-  end,
+  opts = {
+    experimental = {
+      check_rtp_message = false,
+    },
+  },
+  keys = {
+    { '<leader>mp', ':Markview toggle<CR>', desc = 'Markdown Preview', mode = { 'n' } },
+  },
 }
