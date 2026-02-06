41f737dd05ac4b62f8adb645b398bdb3582001cd
jnines <jason.mullenberg@gmail.com>
2026-02-05 20:14:01 -0600

    Whichkey to opts

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,32 +1,32 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  config = function()
-    local wk = require('which-key')
 
-    wk.setup({
-      replace = {
-        key = {
-          { '<tab>', 'TAB' },
-          { '<space>', 'SPC' },
-          { '<leader>', 'SPC' },
-        },
+  opts = {
+    replace = {
+      key = {
+        { '<tab>', 'TAB' },
+        { '<space>', 'SPC' },
+        { '<leader>', 'SPC' },
       },
-    })
-
-    wk.add({
-      { '<leader>b', group = 'Buffers' },
-      { '<leader>c', group = 'Code' },
-      { '<leader>d', group = 'Diagnostics' },
-      { '<leader>f', group = 'File' },
-      { '<leader>g', group = 'GoTo' },
-      { '<leader>h', group = 'Harpoon' },
-      { '<leader>m', group = 'Markdown' },
-      { '<leader>q', '<cmd>xa<CR><cmd>qa<CR>', desc = 'Quit' },
-      { '<leader>r', group = 'Rename' },
-      { '<leader>s', group = 'Splits' },
-      { '<leader>t', group = 'Tabs' },
-      { '<leader>x', group = 'Extras' },
-    })
-  end,
+    },
+    sort = { 'alphanum' },
+    spec = {
+      {
+        mode = { 'n', 'x' },
+        { '<leader>b', group = 'Buffers' },
+        { '<leader>c', group = 'Code' },
+        { '<leader>d', group = 'Diagnostics' },
+        { '<leader>f', group = 'File' },
+        { '<leader>g', group = 'GoTo' },
+        { '<leader>h', group = 'Harpoon' },
+        { '<leader>m', group = 'Markdown' },
+        { '<leader>q', '<cmd>xa<CR><cmd>qa<CR>', desc = 'Quit' },
+        { '<leader>r', group = 'Rename' },
+        { '<leader>s', group = 'Splits' },
+        { '<leader>t', group = 'Tabs' },
+        { '<leader>x', group = 'Extras' },
+      },
+    },
+  },
 }
