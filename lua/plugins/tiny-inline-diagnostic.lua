c0688b474da7a99eabc1463eedc0100a5f5c96a1
jnines <jason.mullenberg@gmail.com>
2026-02-05 19:41:31 -0600

    Tiny-inline-diag to opt

--- lua/plugins/tiny-inline-diagnostic.lua
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -2,8 +2,6 @@ return {
   'rachartier/tiny-inline-diagnostic.nvim',
   event = 'VeryLazy',
   priority = 1000,
-  config = function()
-    require('tiny-inline-diagnostic').setup()
-    vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
-  end,
+  opts = {},
+  vim.diagnostic.config({ virtual_text = false }), -- Disable default virtual text
 }
