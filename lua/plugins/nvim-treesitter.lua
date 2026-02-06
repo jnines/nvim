1ab0d6bfe96261878bfb702b466812263be3efa5
jnines <jason.mullenberg@gmail.com>
2026-02-05 18:49:09 -0600

    Treesitter to opts

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,60 +1,58 @@
----@diagnostic disable: missing-fields
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPre', 'BufNewFile' },
-    build = ':TSUpdate',
-    dependencies = {
-      'tree-sitter/tree-sitter-regex',
-      'nvim-treesitter/nvim-treesitter-context',
-      'OXY2DEV/markview.nvim',
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  event = { 'BufReadPre', 'BufNewFile' },
+  build = ':TSUpdate',
+  dependencies = {
+    'tree-sitter/tree-sitter-regex',
+    'nvim-treesitter/nvim-treesitter-context',
+    'OXY2DEV/markview.nvim',
+  },
+  opts = {
+    highlight = {
+      enable = true,
     },
-    config = function()
-      local treesitter = require('nvim-treesitter')
-
-      local tsctx = require('treesitter-context')
-
-      vim.keymap.set('n', '<leader>gc', function()
-        tsctx.go_to_context()
-      end, { silent = true, desc = 'Goto context' })
-
-      treesitter.setup({
-        highlight = {
-          enable = true,
-        },
-        indent = { enable = true },
-        ensure_installed = {
-          'json',
-          'javascript',
-          'typescript',
-          'tsx',
-          'yaml',
-          'html',
-          'php',
-          'css',
-          'markdown',
-          'markdown_inline',
-          'bash',
-          'lua',
-          'vim',
-          'python',
-          'dockerfile',
-          'gitignore',
-          'go',
-          'qmljs',
-          'qmldir',
-          'query',
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<C-space>',
-            node_incremental = '<C-space>',
-            scope_incremental = false,
-            node_decremental = '<bs>',
-          },
-        },
-      })
-    end,
+    indent = { enable = true },
+    ensure_installed = {
+      'json',
+      'javascript',
+      'typescript',
+      'tsx',
+      'yaml',
+      'html',
+      'php',
+      'css',
+      'markdown',
+      'markdown_inline',
+      'bash',
+      'lua',
+      'vim',
+      'python',
+      'dockerfile',
+      'gitignore',
+      'go',
+      'qmljs',
+      'qmldir',
+      'query',
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>gc',
+      function()
+        require('treesitter-context').go_to_context()
+      end,
+      desc = 'GoTo context',
+      mode = { 'n' },
+    },
   },
 }
