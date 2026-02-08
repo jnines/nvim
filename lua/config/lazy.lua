b36198159d30e5bd379612dde398293a6627a3ba
jnines <jason.mullenberg@gmail.com>
2026-02-08 15:46:24 -0600

    Use profiles for lazy spec

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,7 @@ vim.g.maplocalleader = ' '
 
 -- Setup lazy.nvim
 require('lazy').setup({
-  spec = { import = 'plugins' },
+  spec = require('util.profiles').get_lazy_spec_imports(),
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'habamax' } },
